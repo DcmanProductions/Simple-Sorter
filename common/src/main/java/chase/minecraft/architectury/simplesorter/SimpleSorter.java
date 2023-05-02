057aa247c58964120488217dd9a5cd8bf28f46e2
Drew Chase <dcmanproductions@gmail.com>
2023-05-02 11:22:59 -0400

    Initial commit

---
+++ common/src/main/java/chase/minecraft/architectury/simplesorter/SimpleSorter.java
@@ -0,0 +1,15 @@
+package chase.minecraft.architectury.simplesorter;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class SimpleSorter
+{
+	public static final String MOD_ID = "simple-sorter";
+	public static Logger log = LogManager.getLogManager().getLogger(MOD_ID);
+	
+	public static void init()
+	{
+		
+	}
+}
